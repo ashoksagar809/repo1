9a78d5be1aa2edde9bcf2b5d6badfaa0d0080b88
Ashok sagar <ashoksurampalli809@gmail.com>
2021-05-28 11:05:01 +0530

    finding largst in list

--- branch1file.py
+++ branch1file.py
@@ -1,1 +1,20 @@
-print("hello ashok")
+lis=[20,30,50,80,50,60,90]
+
+print("largest number in the list is :",max(lis))
+
+print("-----------------------------------------------------------")
+
+lis=[]
+count=int(input("how many numbers ?"))
+for x in range(count):
+    number=int(input("Enter number :"))
+    lis.append(number)
+print("largest number in the list is :",max(lis))
+
+print("-----------------------------------------------------------")
+
+lis=[]
+count=int(input("how many numbers? :"))
+for x in range(count):
+    number=int(input("Enter number :"))
+    lis.append(
